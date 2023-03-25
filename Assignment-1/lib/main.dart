fdd3bcb2ddc3b167b91a8b63b57481bfba5c018b
MohamedAhmed1111 <94565947+MohamedAhmed1111@users.noreply.github.com>
2023-03-26 00:33:33 +0200

    Add files via upload

---
+++ Assignment-1/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.brown,
+          title: Text("First Application"),
+        ),
+        body: Center(
+          child: Text("Mohamed Ahmed Abdel Salam"),
+        ));
+  }
+}
